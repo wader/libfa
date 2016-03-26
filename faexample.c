fa8a261bc494933ed8b97d1f7d796c8d2597e8f7
AirVan21 <airvan21@gmail.com>
2016-03-26 23:47:43 +0300

    Revert "Added examples for tasks"
    This reverts commit 6810a3a3d17ee68b9f7d97fcd9db5af87eae55f8.

--- faexample.c
+++ faexample.c
@@ -43,31 +43,42 @@ int main(int argc, char **argv) {
   fa_init();
 
   // create union NFA of two regexp NFAs
-  // (1) fa_t *a_fa = fa_regexp_fa("^(a|b)*ab(a|b)*|(a|b)*a|b*$", &errstr, &errpos, NULL);
-  // (2) fa_t *a_fa = fa_regexp_fa("^(a|b)*(ab|ba)(a|b)*|a*|b*$", &errstr, &errpos, NULL);
-  fa_t *a_fa = fa_regexp_fa("^(_|[a-zA-Z])(_|[a-zA-Z]|[0-9])*$", &errstr, &errpos, NULL);
-
-
+  fa_t *a_fa = fa_regexp_fa("^aa*$", &errstr, &errpos, NULL);
   // assign 0 to all accepting states for first regexp
   fa_set_accepting_opaque(a_fa, (void *)0);
-  // fa_t *b_fa = fa_regexp_fa("^a(a|b)$", &errstr, &errpos, NULL);
+  fa_t *b_fa = fa_regexp_fa("^a(a|b)$", &errstr, &errpos, NULL);
   // assign 1 to all accepting states for second regexp
-  // fa_set_accepting_opaque(b_fa, (void *)1);
-  // fa_t *union_fa = fa_union(a_fa, b_fa);
-  fa_graphviz_output(a_fa, "pics/nfa.dot", NULL);
+  fa_set_accepting_opaque(b_fa, (void *)1);
+  fa_t *union_fa = fa_union(a_fa, b_fa);
+  fa_graphviz_output(union_fa, "union.dot", NULL);
 
   // determinize and choose lowest opaque number when more than one opaque
   // value end up in same accepting state.
   // this happens when two regexps are overlapping.
-  fa_t *dfa = fa_determinize_ex(a_fa, state_pri, NULL, NULL);
-  fa_graphviz_output(dfa, "pics/dfa.dot", NULL);
+  fa_t *dfa = fa_determinize_ex(union_fa, state_pri, NULL, NULL);
+  fa_graphviz_output(dfa, "dfa.dot", NULL);
 
   // minimize and treat states with different opaque values as distinguishable.
   // this is to be able to know which original regexp has a match when using
   // the minimize DFA
   fa_t *mdfa = fa_minimize_ex(dfa, state_cmp, NULL);
   fa_destroy(dfa);
-  fa_graphviz_output(mdfa, "pics/mdfa.dot", NULL);
+  fa_graphviz_output(mdfa, "mdfa.dot", NULL);
+
+  // convert minimal DFA into format suitable for running it on input
+  fa_sim_t *sim = fa_sim_create(mdfa);
+  fa_destroy(mdfa);
+
+  fa_sim_run_t fsr;
+
+  fa_sim_run_init(sim, &fsr);
+  if (fa_sim_run(sim, &fsr, (uint8_t *)"aa", 2) == FA_SIM_RUN_ACCEPT)
+    printf("matches %p\n", fsr.opaque); // outputs matches 0x0
+  fa_sim_run_init(sim, &fsr);
+  if (fa_sim_run(sim, &fsr, (uint8_t *)"ab", 2) == FA_SIM_RUN_ACCEPT)
+    printf("matches %p\n", fsr.opaque); // outputs matches 0x1
+
+  fa_sim_destroy(sim);
 
   return 0;
 }
